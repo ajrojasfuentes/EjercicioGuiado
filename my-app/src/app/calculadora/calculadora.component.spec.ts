4811f5179e3c1d206c0038da4496e6fa73a8f8b9
AnthonyRF00 <anthony.rf00@gmail.com>
2024-10-04 22:45:06 -0600

    Ejercicio guiado finalizado con exito.

--- my-app/src/app/calculadora/calculadora.component.spec.ts
+++ my-app/src/app/calculadora/calculadora.component.spec.ts
@@ -21,3 +21,22 @@ describe('CalculadoraComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('Cancatenar numero', function () { 
+  let component: CalculadoraComponent; 
+  beforeEach(() => { 
+  component = new CalculadoraComponent(); 
+  }); 
+  it("concatenar 2 + 5 = 25", function() { 
+  expect(component.concatenarNumero("2","5")).toBe("25"); 
+  }); 
+  it("concatenar Vacío + . = 0.", function() { 
+  expect(component.concatenarNumero("",".")).toBe("0."); 
+  }); 
+  it("concatenar Decimal 5. + 5 = 5.5", function() { 
+  expect(component.concatenarNumero("5.","5")).toBe("5.5"); 
+  }); 
+  it("concatenar Más de un punto 5.5 + . = 5.5", function() { 
+  expect(component.concatenarNumero("5.5",".")).toBe("5.5"); 
+  }); 
+  }); 
--- my-app/src/app/calculadora/calculadora.service.spec.ts
+++ my-app/src/app/calculadora/calculadora.service.spec.ts
@@ -20,4 +20,41 @@ describe('Sumar', function () {
   CalculadoraService) => { 
   expect(service.sumar(10, 15)).toBe(25); 
   })); 
-}); 
+
+  it('5.5 + 2.3 debe ser 7.8', inject([CalculadoraService], (service: CalculadoraService) => { 
+    expect(service.sumar(5.5, 2.3)).toBe(7.8); 
+  })); 
+});
+
+describe('Restar', function () { 
+  it('2 - 2 debe ser 0', inject([CalculadoraService], (service: CalculadoraService) => { 
+    expect(service.restar(2, 2)).toBe(0); 
+  })); 
+
+  it('3 - 4 debe ser -1', inject([CalculadoraService], (service: CalculadoraService) => { 
+    expect(service.restar(3, 4)).toBe(-1); 
+  })); 
+});
+
+describe('Multiplicar', function () { 
+  it('3 * 2 debe ser 6', inject([CalculadoraService], (service: CalculadoraService) => { 
+    expect(service.multiplicar(3, 2)).toBe(6); 
+  })); 
+});
+
+describe('Dividir', function () { 
+
+  it("2 dividido por 2 debe ser 1", inject([CalculadoraService], 
+  (service: CalculadoraService) => { 
+  expect(service.dividir(2, 2)).toBe(1); 
+  })); 
+
+  it("6 dividido por 0 debe generar una Excepción", 
+  inject([CalculadoraService], (service: CalculadoraService) => { 
+  expect(function () { service.dividir(6, 0); }).toThrowError(Error, 'División por cero'); 
+  expect(function () { service.dividir(6, 0); }).toThrowError('División por cero'); 
+  expect(function () { service.dividir(6, 0); }).toThrowError(Error); 
+  expect(function () { service.dividir(6, 0); 
+  }).toThrowError(/División por cero/); 
+  })); 
+  }); 
